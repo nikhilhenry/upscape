cd93531022aaa2a78ec00d7c0f1d954f24769492
Nikhil <nikhilanthonyhenry@gmail.com>
2021-03-30 09:29:49 +0530

    add createuser

---
+++ controllers/user.go
@@ -0,0 +1,73 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+	"upscape/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var validate *validator.Validate = validator.New()
+
+func CreateUser(client *mongo.Database) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// establish connection
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		collection := client.Collection("users")
+
+		// check if user document already exists
+		cursor, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer cursor.Close(ctx)
+		if cursor.RemainingBatchLength() > 0 {
+			fmt.Println("user exists")
+			c.JSON(http.StatusConflict, gin.H{"error": "user exists"})
+			return
+		}
+
+		// create a user variable
+		var user models.User
+
+		// bind object
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// validate input
+		if validationErr := validate.Struct(user); validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		// hash password
+		bytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+		if err != nil {
+			log.Fatal(err.Error())
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		user.Password = string(bytes)
+
+		// post do Database
+		result, insertErr := collection.InsertOne(ctx, user)
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": insertErr.Error()})
+			return
+		}
+
+		// return the object
+		c.JSON(http.StatusOK, result)
+	}
+}
