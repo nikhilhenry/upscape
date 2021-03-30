663b20f40f8abd5332306e9211339e6ba103d6e0
Nikhil <nikhilanthonyhenry@gmail.com>
2021-03-30 10:18:33 +0530

    creat login route

--- controllers/login.go
+++ controllers/login.go
@@ -1,11 +1,19 @@
 package controllers
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
 	"time"
+	"upscape/models"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginRequest struct {
@@ -28,4 +36,53 @@ func generateToken(userId string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func LoginUser(client *mongo.Database) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		// establish connection
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		collection := client.Collection("users")
+
+		var loginRequest LoginRequest
+
+		// bind object
+		if err := c.BindJSON(&loginRequest); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// validate request
+		validate := validator.New()
+		if validationErr := validate.Struct(loginRequest); validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		// query database
+		var user models.User
+		err := collection.FindOne(ctx, bson.M{}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login failed"})
+			return
+		}
+
+		// check password hash
+		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password))
+		if err != nil {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Incorrect email or password."})
+			return
+		}
+
+		// generate token
+		token, err := generateToken(user.ID.Hex())
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"token": token})
+	}
 }
