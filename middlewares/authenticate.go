2d5bd63289e0a23657b8c5ba8540f8f8172f217c
Nikhil <nikhilanthonyhenry@gmail.com>
2021-03-30 13:15:30 +0530

    create auth middleware

---
+++ middlewares/authenticate.go
@@ -0,0 +1,60 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+type Header struct {
+	Token string `header:"auth_token"`
+}
+
+type Claim struct {
+	UserId string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+func IsAuthenticated(c *gin.Context) {
+
+	// get token from header
+	header := Header{}
+	if err := c.ShouldBindHeader(&header); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+	}
+
+	// check if token present
+	if len(header.Token) < 1 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "no token sent"})
+		return
+	}
+
+	// extract token from jwt
+	token, err := jwt.ParseWithClaims(header.Token, &Claim{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There is an error")
+		}
+		return []byte(os.Getenv("SECRE")), nil
+	})
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if !token.Valid {
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "not authorised"})
+		return
+	}
+
+	// if the token is valid
+	if claims, ok := token.Claims.(*Claim); ok && token.Valid {
+		c.Set("user_id", claims.UserId)
+	}
+
+	c.Next()
+
+}
