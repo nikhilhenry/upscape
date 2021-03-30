4d679b2e68337efff6f1a14ab5a6adbc5108b741
Nikhil <nikhilanthonyhenry@gmail.com>
2021-03-30 08:31:58 +0530

    create web server

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// load env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Starting the application")
+
+	// instantiate router
+	router := gin.Default()
+
+	// declare routes
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome to Upscape!"})
+	})
+
+	router.Run()
 }
