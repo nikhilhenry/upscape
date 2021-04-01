252867a35f5d16ad6c99b3fd838ac5b036570828
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-01 09:24:24 +0530

    fix validation errors

--- models/Task.go
+++ models/Task.go
@@ -8,9 +8,9 @@ import (
 
 type Task struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name        string             `json:"name,omitempty" validate:"required,alphanum" bson:"name,omitempty"`
+	Name        string             `json:"name,omitempty" validate:"required,max=20" bson:"name,omitempty"`
 	Duration    int                `json:"duration,omitempty" bson:"duration,omitempty"`
-	Completed   bool               `json:"completed,omitempty" validate:"required" bson:"completed"`
+	Completed   bool               `json:"completed" bson:"completed"`
 	TagIDs      []string           `json:"tag_ids" bson:"tag_ids"`
 	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
 	CompletedAt time.Time          `json:"completed_at" bson:"completed_at"`
