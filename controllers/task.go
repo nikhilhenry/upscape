887a9aa28923b400cca2e6d0bd7513e12c7bc272
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-01 09:34:27 +0530

    create get tasks

--- controllers/task.go
+++ controllers/task.go
@@ -7,8 +7,45 @@ import (
 	"upscape/models"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+// get tasks
+func GetTasks(client *mongo.Database) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// establish connection
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		collection := client.Collection("tasks")
+
+		// query Database
+		cursor, err := collection.Find(ctx, bson.M{})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		var tasks []models.Task
+
+		// loop through results
+		defer cursor.Close(ctx)
+		for cursor.Next(ctx) {
+			var task models.Task
+			cursor.Decode(&task)
+			tasks = append(tasks, task)
+		}
+
+		if err := cursor.Err(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// send result
+		c.JSON(http.StatusOK, tasks)
+	}
+}
 
 // create task
 func CreateTask(client *mongo.Database) gin.HandlerFunc {
