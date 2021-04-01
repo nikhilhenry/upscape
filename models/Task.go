cc496e75685521adb6dc4751eeaf5203f5758186
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-01 13:16:16 +0530

    add fields to task model

--- models/Task.go
+++ models/Task.go
@@ -8,9 +8,12 @@ import (
 
 type Task struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Oid         int                `json:"id" bson:"id"`
 	Name        string             `json:"name,omitempty" validate:"required,max=20" bson:"name,omitempty"`
 	Duration    int                `json:"duration,omitempty" bson:"duration,omitempty"`
 	Completed   bool               `json:"completed" bson:"completed"`
+	Highlight   bool               `json:"highlight,omitempty" bson:"highlight,omitempty"`
+	IsTomorrow  bool               `json:"is_tomorrow,omitempty" bson:"is_tomorrow,omitempty"`
 	TagIDs      []string           `json:"tag_ids" bson:"tag_ids"`
 	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
 	CompletedAt time.Time          `json:"completed_at,omitempty" bson:"completed_at,omitempty"`
