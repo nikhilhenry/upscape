cf701f310ed24e3e4631d5541b6b90ef14face32
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-01 09:18:11 +0530

    implement task route

--- main.go
+++ main.go
@@ -55,6 +55,8 @@ func main() {
 	router.POST("/api/login", controllers.LoginUser(clientDatabase))
 	router.PUT("/api/user", middlewares.IsAuthenticated, controllers.UpdateUser(clientDatabase))
 	router.PUT("/api/user/password", middlewares.IsAuthenticated, controllers.UpdateUserPassword(clientDatabase))
+	router.POST("/api/task", middlewares.IsAuthenticated, controllers.CreateTask(clientDatabase))
+
 	// @todo remove this request
 	router.GET("/howdy", middlewares.IsAuthenticated, func(c *gin.Context) {
 		user_id := c.MustGet("user_id").(string)
