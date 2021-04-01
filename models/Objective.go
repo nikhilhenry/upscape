7374b9ab68cf8faf0475fb0da1f8dbccabb4c98f
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-01 20:25:03 +0530

    fix objective model

--- models/Objective.go
+++ models/Objective.go
@@ -8,9 +8,9 @@ import (
 
 type Objective struct {
 	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name         string             `json:"name,omitempty" validate:"required,max=20,alphanum" bson:"name,omitempty"`
-	Completed    bool               `json:"completed,omitempty" validate:"required" bson:"completed,omitempty"`
-	Body         string             `json:"body,omitempty" validate:"required,max=100,alphanum" bson:"body,omitempty"`
-	ScheduledFor time.Time          `json:"scheduledFor,omitempty" bson:"scheduledFor,omitempty"`
-	CreatedAt    time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	Name         string             `json:"name,omitempty" validate:"required,max=20" bson:"name,omitempty"`
+	Completed    bool               `json:"completed,omitempty" bson:"completed,omitempty"`
+	Body         string             `json:"body,omitempty" validate:"max=100" bson:"body,omitempty"`
+	ScheduledFor time.Time          `json:"scheduled_for,omitempty" bson:"scheduled_for,omitempty"`
+	CreatedAt    time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
