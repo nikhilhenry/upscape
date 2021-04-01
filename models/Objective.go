345ef6b635a14b40474ea99702c40550e79c2b1d
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-01 20:52:14 +0530

    remove omitempty for completed in objective

--- models/Objective.go
+++ models/Objective.go
@@ -9,7 +9,7 @@ import (
 type Objective struct {
 	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name         string             `json:"name,omitempty" validate:"required,max=20" bson:"name,omitempty"`
-	Completed    bool               `json:"completed,omitempty" bson:"completed,omitempty"`
+	Completed    bool               `json:"completed" bson:"completed"`
 	Body         string             `json:"body,omitempty" validate:"max=100" bson:"body,omitempty"`
 	ScheduledFor time.Time          `json:"scheduled_for,omitempty" bson:"scheduled_for,omitempty"`
 	CreatedAt    time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
