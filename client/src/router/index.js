7789502e1eb481b2f1ccbdbe64e289b2f37e3ebe
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-23 11:08:20 +0530

    create router guards

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 
+import store from '@/store'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -30,6 +32,48 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// router gaurds
+router.beforeEach((to,from,next)=>{
+
+  // get access token from store
+  
+  const isAuthenticated = store.getters['user/getAuthenticatedState']
+
+  // if auth is required
+  if(to.matched.some(record=>record.meta.requiresAuth)){
+    // check if user is authenticated
+    if(isAuthenticated) next()
+    // if not authenticated
+    else{
+      next({
+        path:'/login',
+        query:{
+          redirect: to.fullPath
+        }
+      })
+    }
+  }
+  // if guest is required
+  else if(to.matched.some(record=>record.meta.requiresGuest)){
+    // check if user is not authenticated
+    if(!isAuthenticated) next()
+    // if authenticated
+    else{
+      next({
+        path:'/',
+        query:{
+          redirect:to.fullPath
+        }
+      })
+    }
+  }
+
+  // if nothing required
+  else{
+    next()
+  }
 })
 
 export default router
