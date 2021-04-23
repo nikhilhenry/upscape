19cce1de33d2c6825af96539bb949525dd2b4601
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-23 21:53:22 +0530

    query api for user meta

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -8,8 +8,23 @@
 </template>
 
 <script>
+import metaQuery from '@/api/metaQuery'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Home',
+  methods:{
+    getMeta: async function(){
+      const response = await metaQuery()
+      this.storeMetaData(response)
+      
+    },
+    ...mapActions({ 
+      storeMetaData:'user/storeMetaData'
+    })
+  },
+  created(){
+    this.getMeta()
+  }
 }
 </script>
