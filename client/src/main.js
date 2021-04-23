9e937328552e9e1768f7aebef243f6308dd191a6
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-23 09:59:35 +0530

    set axios default

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// external libraries
+import axios from 'axios'
+
+// axios baseURL
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
+
 Vue.config.productionTip = false
 
 new Vue({
