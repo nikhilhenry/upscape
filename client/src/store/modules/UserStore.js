182c08c5ca84df4f93caac2346482e1824c4cafe
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-23 09:54:33 +0530

    create userstore module

---
+++ client/src/store/modules/UserStore.js
@@ -0,0 +1,56 @@
+export default{
+  namespaced:true,
+
+  state:{
+    auth_token:localStorage.getItem('auth_token') || '',
+    isAuthenticated:false,
+    profilePic:'',
+    fullname:''
+  },
+
+  mutations:{
+    STORE_AUTH_TOKEN:(state,userToken)=>{
+      state.auth_token = userToken;
+    },
+    SET_AUTHENTICATED:(state,isAuthenticated)=>{
+      state.isAuthenticated=isAuthenticated;
+    },
+    STORE_FULL_NAME:(state,userFullName)=>{
+      state.fullname = userFullName
+    },
+    STORE_PROFILE_PIC:(state,userProfilePic)=>{
+      state.profilePicUrl = userProfilePic
+    },    
+  },
+
+  actions:{
+    // store token
+    storeAuthToken:(context,userToken)=>{
+
+      // store token to local storage
+      localStorage.setItem('auth_token',userToken);
+      
+      context.commit('STORE_AUTH_TOKEN',userToken);
+
+      // @todo: set axios headers
+
+      // set authenticated status
+      context.commit("SET_AUTHENTICATED",true);
+    }
+  },
+
+  getters:{
+    getAuthToken:state=>{
+      return state.auth_token
+    },
+    getAuthenticatedState:state=>{
+      return state.isAuthenticated
+    },
+    getFirstName:state=>{
+      return state.fullname.split(" ")[0];
+    },
+    getProfilePicURL:state=>{
+      return state.profilePicUrl
+    },
+  }
+}
