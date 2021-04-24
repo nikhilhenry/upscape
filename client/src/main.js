44aef76d572586b1db3f07c8055ff66405819329
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-24 18:59:49 +0530

    set timeAgo locale

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,15 @@ import store from './store'
 
 // external libraries
 import axios from 'axios'
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+
+// requirement to set locale
+TimeAgo.addDefaultLocale(en)
 
 // axios baseURL
 axios.defaults.baseURL = process.env.VUE_APP_API_URL
+
 
 Vue.config.productionTip = false
 
