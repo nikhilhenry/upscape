b5119a8f4376e840fc4cc95ebab082e9c3b4b12a
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-24 19:16:19 +0530

    query api for tasks

--- client/src/views/Tasks.vue
+++ client/src/views/Tasks.vue
@@ -26,6 +26,8 @@ import Avatar from '@/components/Avatar'
 import TasksDateRange from '@/components/TasksDateRange'
 import TaskItem from '@/components/TaskItem'
 
+import getTasks from '@/api/taskGet'
+
 export default {
   name:'Tasks',
   components:{
@@ -36,51 +38,20 @@ export default {
   data(){
     return{
       queryDate:'today',
-      tasks:[
-  {
-    "_id": "6065441b2188168ab6e0c47a",
-    "id": 1,
-    "name": "Eat sushi",
-    "duration": 60,
-    "completed": false,
-    "created_at": "2021-04-24T13:31:09Z",
-    "completed_at": "0001-01-01T00:00:00Z",
-    "highlight":true,
-  },
-  {
-    "_id": "606543dbb8bd696647d73d61",
-    "id": 3,
-    "name": "Eat pizza",
-    "duration": 10,
-    "completed": false,
-    "created_at": "2021-04-01T03:54:03Z",
-    "completed_at": "0001-01-01T00:00:00Z"
-  },
-  {
-    "_id": "606546f0c5d068ebc0eb3afe",
-    "id": 4,
-    "name": "Eat pasta",
-    "duration": 5,
-    "completed": false,
-    "created_at": "2021-04-01T04:07:12Z",
-    "completed_at": "0001-01-01T00:00:00Z"
-  },
-  {
-    "_id": "60658b44229c35fa4eb2f160",
-    "id": 11,
-    "name": "the present",
-    "duration": 10,
-    "completed": true,
-    "created_at": "2021-04-01T04:07:12Z",
-    "completed_at": "2021-04-01T14:23:46Z"
-  }
-]    
+      tasks:[]   
     }
   },
   methods:{
     test:function(queryDate){
       console.log(queryDate)
+    },
+    queryTasks:async function(){
+      const tasks = await getTasks(this.dateQuery)
+      this.tasks = tasks
     }
+  },
+  mounted(){
+    this.queryTasks()
   }
 }
 </script>
