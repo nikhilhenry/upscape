748784aa3547fdfe3d27f367feb702925718dd23
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-24 12:49:50 +0530

    create dummy taskitem

---
+++ client/src/components/TaskItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="task">
+    <div class="wrapper">
+      <div class="left">
+        <p class="title">Eat Sushi</p>
+        <span class="date">1 hour ago</span>
+      </div>
+      <div class="right">
+        <span class="duration">60 MIN</span>
+        <i class="fas fa-star"></i>
+        <input id="c1" type="checkbox">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TaskItem'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/toggles.scss"
+</style>
