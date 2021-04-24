804ec3cc7979c61bd56c637bacdd7bd8e33d407c
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-24 23:10:24 +0530

    create modal view component

---
+++ client/src/components/ModalView.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="modal is-clipped" :class="{'is-active':isActive}">
+  <div class="modal-background" @click="setModalStatus"></div>
+  <div class="modal-content">
+    <slot></slot>
+  </div>
+  <button class="modal-close is-large" aria-label="close" @click="setModalStatus"></button>
+</div>
+</template>
+
+<script>
+export default {
+  name:'ModalView',
+  data(){
+    return{
+      isActive:true
+    }
+  },
+  methods:{
+    setModalStatus(){
+      this.isActive = !this.isActive
+
+      //send back if not active
+      if(!this.isActive) this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~bulma";
+
+.modal-content{
+  overflow: visible;
+}
+
+@media only screen and (min-width: 1024px) {
+.modal-content{
+  width: auto;
+}
+}
+</style>
