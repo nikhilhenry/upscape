de6b4534de2c2946bdeda67deb51f783feb32eab
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-25 13:24:48 +0530

    create load tags mixin

---
+++ client/src/mixins/loadTags.js
@@ -0,0 +1,14 @@
+import getTags from '@/api/tagGet.js'
+
+export default{
+  async mounted(){
+    // check if tags isLoaded
+    if(!this.$store.tags.tagsLoaded){
+      // query tags
+      const tags = await getTags()
+
+      // save tags to store
+      this.$store.dispatch('tag/storeTags',tags)
+    }
+  }
+}
