defc5a04320a2b909d684e3ee63d6105ade13f84
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-25 18:39:01 +0530

    create dummy objective list

--- client/src/views/Objectives.vue
+++ client/src/views/Objectives.vue
@@ -6,17 +6,35 @@
       <h1 class="title">Objectives</h1>
       <Avatar/>
     </div>
+
+    <!-- objectives -->
+    <ul class="objective-list">
+      <li>
+        <ObjectiveItem/>
+      </li>
+    </ul>
   </div>
 </div>
 </template>
 
 <script>
 import Avatar from '@/components/Avatar'
+import ObjectiveItem from '@/components/ObjectiveItem'
 
 export default {
   name:'Objectives',
   components:{
-    Avatar
+    Avatar,
+    ObjectiveItem
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.objective-list{
+  margin-top: 3rem;
+  list-style: none;
+  margin-left:0;
+  padding:0;
+}
+</style>
