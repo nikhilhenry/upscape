a03f2f25bec8c4307f2344d668de533ec2264080
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-25 17:22:58 +0530

    add form elements

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -1,9 +1,96 @@
 <template>
-  <h1>Settings</h1>
+<div class="home">
+  <div class="container">
+    <!-- title bar -->
+    <div class="title-bar">
+      <h1 class="title">Settings</h1>
+      <Avatar/>
+    </div>
+      <section class="options">
+        <!-- name -->
+        <div class="option">
+          <h3 class="title is-4">Change Name</h3>
+          <div class="field has-addons">
+            <div class="control">
+              <input class="input" type="text" placeholder="New Name">
+            </div>
+            <div class="control">
+              <a class="button is-info">
+                Update
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <!-- img url -->
+        <div class="option">
+          <h3 class="title is-4">Change Image URL</h3>
+          <div class="field has-addons">
+            <div class="control">
+              <input class="input" type="text" placeholder="https://github.com">
+            </div>
+            <div class="control">
+              <a class="button is-info">
+                Update
+              </a>
+            </div>
+          </div>
+        </div>        
+
+        <div class="option logout">
+          <button class="button is-danger">
+            <span>Logout</span>
+            <span class="icon">
+              <i class="fas fa-sign-out-alt"></i>
+            </span>
+          </button>          
+        </div>
+      </section>    
+  </div>
+</div>
 </template>
 
 <script>
+import Avatar from '@/components/Avatar'
+
 export default {
-  name:'Settings'
+  name:'Settings',
+  component:{
+    Avatar
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "~bulma";
+
+.title{
+  color:$text-primary;
+}
+
+.options{
+  margin-top:3rem;
+
+  .option{
+    margin-bottom: 2rem;
+    .button-main{
+      margin:1.5rem 0 1rem;
+      padding:.3rem 2rem .3rem;
+    }
+  }
+}
+
+.input{
+  background:$secondary;
+  border-color: darken($secondary,5%);
+  color:$text-secondary;
+  &::placeholder{
+    color:darken($text-primary,20%);
+  }
+}
+
+.logout{
+  position:fixed;
+  bottom:calc(5rem + 20px);
+}
+</style>
