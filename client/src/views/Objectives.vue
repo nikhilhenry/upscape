2cafb38dd8309995c7904b43cea7997089ae9cc0
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-25 18:28:10 +0530

    add styling to objectives page

--- client/src/views/Objectives.vue
+++ client/src/views/Objectives.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Objectives</h1>
+<div class="home">
+  <div class="container">
+    <!-- title bar -->
+    <div class="title-bar">
+      <h1 class="title">Objectives</h1>
+      <Avatar/>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
+import Avatar from '@/components/Avatar'
+
 export default {
-  name:'Objectives'
+  name:'Objectives',
+  components:{
+    Avatar
+  }
 }
 </script>
