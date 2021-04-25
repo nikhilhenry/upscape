ade24878f264c911fe9a9c5266f73fdc61075443
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-25 13:30:22 +0530

    add computed getter

--- client/src/mixins/loadTags.js
+++ client/src/mixins/loadTags.js
@@ -1,6 +1,13 @@
 import getTags from '@/api/tagGet.js'
+import  {mapGetters} from 'vuex'
 
 export default{
+
+  computed:{
+    ...mapGetters({
+      tags:'tag/getTags'
+    })
+  },
   async mounted(){
     // check if tags isLoaded
     if(!this.$store.state.tag.tagsLoaded){
