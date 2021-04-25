7085ec16279daf4d03e516ee5d2573882df9260b
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-25 10:12:01 +0530

    fix typo

--- client/src/store/modules/TaskStore.js
+++ client/src/store/modules/TaskStore.js
@@ -50,7 +50,7 @@ export default{
 
     deleteTask:(context,taskId)=>{
       if(taskId){
-        const index = context.state.taks.findIndex(task => task._id === taskId)
+        const index = context.state.tasks.findIndex(task => task._id === taskId)
         context.commit("DELETE_TASK",index)
       }
     }
