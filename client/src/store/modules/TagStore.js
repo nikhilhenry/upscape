2bb97d54eaa980eeff0aa80d4985cd50b81968c4
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-25 13:09:36 +0530

    create tag store

---
+++ client/src/store/modules/TagStore.js
@@ -0,0 +1,46 @@
+export default{
+  namespaced:true,
+
+  state:{
+    tags:[],
+    tagsLoaded:false,
+  },
+
+  mutations:{
+    STORE_TAGS:(state,tagList)=>{
+      const tags = state.tags.concat(tags)
+      state.tags = tags
+    },
+    STORE_TAG:(state,newTag)=>{
+      state.tags.push(newTag)
+    },
+    DELETE_TAG:(state,tagIndex)=>{
+      state.tags.splice(tagIndex,1)
+    },
+    SET_TAGS_LOADED:(state,isLoaded)=>{
+      state.tagsLoaded = isLoaded
+    }
+  },
+
+  actions:{
+    storeTags:(context,userTags)=>{
+      context.commit("STORE_TAGS",userTags)
+      if(userTags.length) context.commit("SET_TAGS_LOADED",true)
+    },
+    storeTag:(context,newTag)=>{
+      if(newTag) context.commit("STORE_TAG",newTag)
+    },
+    deleteTag:(context,tagId)=>{
+      if(tagId){
+        const index = context.state.tasks.findIndex(tag => tag._id === tagId)
+        context.commit("DELETE_TAG",index)
+      }
+    }
+  },
+
+  getters:{
+    getTags:(state)=>{
+      return state.tags
+    }
+  }
+}
