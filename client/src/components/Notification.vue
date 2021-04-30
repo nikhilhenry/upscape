222cc8c87c27994092271d96dc376a9a4ab3be59
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-30 14:24:03 +0530

    add styling

--- client/src/components/Notification.vue
+++ client/src/components/Notification.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="notification">
-    <div class="wrapper">
-      <h2 class="message">{{notification.message}}</h2>
+    <div class="wrapper" :class="notification.success ? 'is-success':'is-danger'">
+        <i class="far fa-check-circle" v-if="notification.success"></i>
+        <i class="fas fa-times-circle" v-else></i>
+        <h3 class="message">{{notification.message}}</h3>
     </div>
   </div>
 </template>
@@ -22,6 +24,31 @@ export default {
 
 .wrapper{
   margin:1rem;
+  padding: .5rem 1.2rem .5rem;
+  color: #ffffff;
+  display:flex;
+  align-items: center;
+  // justify-content:space-around;
+  min-width: 200px;
+  height:50px;
+  background-color:$nav-bg-primary;
+  border-radius: 1rem;
+
+  i{
+    font-size: 1.5rem;
+  }
+
+  .message{
+    margin-left: 10px;
+  }
+}
+
+.is-success{
+  background-color:#48c78e!important;
+}
+
+.is-danger{
+  background-color:#f14668!important;
 }
 </style>
 
