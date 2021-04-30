eced10e3dbee71e73da8c354ab92f39f039df2d3
Nikhil <nikhilanthonyhenry@gmail.com>
2021-04-30 13:54:59 +0530

    create base notification component

---
+++ client/src/components/Notification.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="notification">
+    <div class="wrapper">
+      <h2 class="message">{{notification.message}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Notification',
+  props:["notification"]
+}
+</script>
+<style lang="scss" scoped>
+.notification{
+  position: fixed;
+  right:0;
+  bottom:0;
+  z-index: 100;
+}
+
+.wrapper{
+  margin:1rem;
+}
+</style>
+
