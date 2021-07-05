4e6261bda06d392550490e6a35495f6405dba9e7
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-07-05 16:26:53 +0530

    add icon colors

--- client/src/components/TaskTimer.vue
+++ client/src/components/TaskTimer.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="task-timer">
     <section class="icons">
-      <i class="las la-braille"></i>
-      <i class="las la-pause"></i>
-      <i class="las la-undo-alt"></i>
-      <i class="las la-stop"></i>
+      <div class="icon meta">
+        <i class="las la-braille"></i>
+      </div>
+      <div class="icon pause">
+        <i class="las la-pause"></i>
+      </div>
+      <div class="icon retry">
+        <i class="las la-undo-alt"></i>
+      </div>
+      <div class="icon stop">
+        <i class="las la-stop"></i>
+      </div>
     </section>
   </div>
 </template>
@@ -16,11 +24,37 @@
   bottom: 50px;
   left: 50%;
   transform: translate(-50%, 0);
+  background-color: $nav-bg-primary;
+  border-radius: 0.5rem;
 }
 
 .icons {
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  .icon {
+    font-size: 1.5rem;
+    font-weight: 900;
+    padding: 1rem 1.5rem;
+    &:hover {
+      background: $nav-bg-secondary;
+    }
+  }
+}
+
+// icon colors
+.meta {
+  color: $text-primary;
+  filter: grayscale(100%) opacity(0.7);
+}
+.pause {
+  color: $primary;
+}
+.retry {
+  color: #4ad66d;
+}
+.stop {
+  color: #f3722c;
 }
 </style>
