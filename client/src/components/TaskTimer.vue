08754c6cc185f2f33aed11c38f46f45376f666b1
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-07-05 16:11:50 +0530

    create dummy component

---
+++ client/src/components/TaskTimer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="task-timer">
+    <section class="icons">
+      <i class="las la-braille"></i>
+      <i class="las la-pause"></i>
+      <i class="las la-undo-alt"></i>
+      <i class="las la-stop"></i>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.task-timer {
+  position: fixed;
+  position: fixed;
+  bottom: 50px;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+
+.icons {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
