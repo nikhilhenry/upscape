3cab254a35f826605a8f8237494eba7e11cfe1e8
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-07-09 20:15:30 +0530

    lint

--- client/src/store/modules/ObjectiveStore.js
+++ client/src/store/modules/ObjectiveStore.js
@@ -1,68 +1,74 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-export default{
-  namespaced:true,
+export default {
+  namespaced: true,
 
-  state:{
-    objectives:[],
-    objectivesLoaded:false,
+  state: {
+    objectives: [],
+    objectivesLoaded: false,
   },
 
-  mutations:{
-    STORE_OBJECTIVES:(state,objectivesList)=>{
-      const objectives = state.objectives.concat(objectivesList)
-      state.objectives = objectives
+  mutations: {
+    STORE_OBJECTIVES: (state, objectivesList) => {
+      const objectives = state.objectives.concat(objectivesList);
+      state.objectives = objectives;
     },
-    SET_OBJECTIVES_LOADED:(state,isLoaded)=>{
-      state.objectivesLoaded = isLoaded
+    SET_OBJECTIVES_LOADED: (state, isLoaded) => {
+      state.objectivesLoaded = isLoaded;
     },
-    UPDATE_OBJECTIVE:(state,payload)=>{
-      Vue.set(state.objectives,payload.index,payload.updatedObjective)
+    UPDATE_OBJECTIVE: (state, payload) => {
+      Vue.set(state.objectives, payload.index, payload.updatedObjective);
     },
-    STORE_OBJECTIVE:(state,newObjective)=>{
-      state.objectives.push(newObjective)
+    STORE_OBJECTIVE: (state, newObjective) => {
+      state.objectives.push(newObjective);
     },
-    DELETE_OBJECTIVE:(state,objectiveIndex)=>{
-      state.objectives.splice(objectiveIndex,1)
-    }
+    DELETE_OBJECTIVE: (state, objectiveIndex) => {
+      state.objectives.splice(objectiveIndex, 1);
+    },
   },
 
-  actions:{
-    storeObjectives:(context,userTasks)=>{
-      context.commit("STORE_OBJECTIVES",userTasks)
-      if(userTasks.length){
-        context.commit("SET_OBJECTIVES_LOADED",true)
+  actions: {
+    storeObjectives: (context, userTasks) => {
+      context.commit("STORE_OBJECTIVES", userTasks);
+      if (userTasks.length) {
+        context.commit("SET_OBJECTIVES_LOADED", true);
       }
     },
 
-    updateObjective:(context,updatedObjective)=>{
-      if(updatedObjective){
-        const index = context.state.objectives.findIndex(objective=>objective._id === updatedObjective._id)
-        context.commit("UPDATE_OBJECTIVE",{index,updatedObjective})
+    updateObjective: (context, updatedObjective) => {
+      if (updatedObjective) {
+        const index = context.state.objectives.findIndex(
+          (objective) => objective._id === updatedObjective._id
+        );
+        context.commit("UPDATE_OBJECTIVE", { index, updatedObjective });
       }
     },
 
-    storeObjective:(context,newObjective)=>{
-      if(newObjective){
-        context.commit("STORE_OBJECTIVE",newObjective)
+    storeObjective: (context, newObjective) => {
+      if (newObjective) {
+        context.commit("STORE_OBJECTIVE", newObjective);
       }
     },
 
-    deleteObjective:(context,objectiveId)=>{
-      if(objectiveId){
-        const index = context.state.objectives.findIndex(objective => objective._id === objectiveId)
-        context.commit("DELETE_OBJECTIVE",index)
+    deleteObjective: (context, objectiveId) => {
+      if (objectiveId) {
+        const index = context.state.objectives.findIndex(
+          (objective) => objective._id === objectiveId
+        );
+        context.commit("DELETE_OBJECTIVE", index);
       }
-    },    
+    },
   },
 
-  getters:{
-    getObjectives:(state)=>{
-      return state.objectives
+  getters: {
+    getObjectives: (state) => {
+      return state.objectives;
     },
-    getObjective:(state) => (objectiveId)=>{
-      const objective = state.objectives.find(objective => objective._id === objectiveId)
-      return objective
-    }
-  }
-}
+    getObjective: (state) => (objectiveId) => {
+      const objective = state.objectives.find(
+        (objective) => objective._id === objectiveId
+      );
+      return objective;
+    },
+  },
+};
