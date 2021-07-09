020551e7503b9d22dde94e33e04d93e4e1680ccc
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-07-09 17:35:13 +0530

    lint

--- client/src/views/Objectives.vue
+++ client/src/views/Objectives.vue
@@ -1,98 +1,100 @@
 <template>
-<div class="home">
-  <div class="container">
-    <!-- title bar -->
-    <div class="title-bar">
-      <h1 class="title">Objectives</h1>
-      <Avatar/>
-    </div>
+  <div class="home">
+    <div class="container">
+      <!-- title bar -->
+      <div class="title-bar">
+        <h1 class="title">Objectives</h1>
+        <Avatar />
+      </div>
 
-    <!-- objectives -->
-    <ul class="objective-list">
-      <li v-for="(objective,index) in objectives" :key="index">
-        <ObjectiveItem :objective="objective"/>
-      </li>
-    </ul>
-  </div>
+      <!-- objectives -->
+      <ul class="objective-list">
+        <li v-for="(objective, index) in objectives" :key="index">
+          <ObjectiveItem :objective="objective" />
+        </li>
+      </ul>
+    </div>
     <!-- create objective objective -->
-  <div class="fab">
-    <router-link :to="{path:'objectives/create'}" class="create-button">Create Objective</router-link>
+    <div class="fab">
+      <router-link :to="{ path: 'objectives/create' }" class="create-button"
+        >Create Objective</router-link
+      >
+    </div>
+    <router-view></router-view>
   </div>
-  <router-view></router-view>  
-</div>
 </template>
 
 <script>
-import Avatar from '@/components/Avatar'
-import ObjectiveItem from '@/components/ObjectiveItem'
-import {mapActions,mapGetters} from 'vuex'
+import Avatar from "@/components/Avatar";
+import ObjectiveItem from "@/components/ObjectiveItem";
+import { mapActions, mapGetters } from "vuex";
 
-import getObjectives from '@/api/objectiveGet.js'
+import getObjectives from "@/api/objectiveGet.js";
 
 export default {
-  name:'Objectives',
-  components:{
+  name: "Objectives",
+  components: {
     Avatar,
-    ObjectiveItem
+    ObjectiveItem,
   },
-  computed:{
-    isLoaded(){
+  computed: {
+    isLoaded() {
       return this.$store.state.objective.objectivesLoaded;
     },
     ...mapGetters({
-      objectives:'objective/getObjectives'
-    })
+      objectives: "objective/getObjectives",
+    }),
   },
-  methods:{
-    queryObjectives:async function(){
-      const objectives = await getObjectives()
-      return objectives
+  methods: {
+    queryObjectives: async function() {
+      const objectives = await getObjectives();
+      return objectives;
     },
-    saveObjectivesToStore:function(objectives){
-      this.storeObjectives(objectives)
+    saveObjectivesToStore: function(objectives) {
+      this.storeObjectives(objectives);
     },
     ...mapActions({
-      storeObjectives:'objective/storeObjectives'
-    })
+      storeObjectives: "objective/storeObjectives",
+    }),
   },
-  async mounted(){
-    if(!this.isLoaded){
-      const objectives = await this.queryObjectives()
-      this.saveObjectivesToStore(objectives)
+  async mounted() {
+    if (!this.isLoaded) {
+      const objectives = await this.queryObjectives();
+      this.saveObjectivesToStore(objectives);
     }
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.objective-list{
+.objective-list {
   margin-top: 3rem;
   list-style: none;
-  margin-left:0;
-  padding:0;
+  margin-left: 0;
+  padding: 0;
 }
 
-.fab{
+.fab {
   position: fixed;
   bottom: 50px;
-  right:calc(50% - 7rem);
+  right: calc(50% - 7rem);
   // right: 50%;
 }
 
-.create-button{
-  color:#fff;
-  background-color:$primary;
-  border:none;
+.create-button {
+  color: #fff;
+  background-color: $primary;
+  border: none;
   font-size: 1.5rem;
   font-family: "avenir";
-  padding: .5rem 1.2rem .5rem;
-  border-radius: .2rem;
-  margin:0;
+  padding: 0.5rem 1.2rem 0.5rem;
+  border-radius: 0.2rem;
+  margin: 0;
   text-decoration: none;
   transition: all 200ms ease-in;
-  &:hover{
+  &:hover {
     // display:none;
-    background-color:darken($primary,5%)
+    background-color: darken($primary, 5%);
   }
 }
 </style>
