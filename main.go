760a72dba221c0820a96cc65434895c6ac568b22
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-08-27 13:49:13 +0530

    reconfigure logger

--- main.go
+++ main.go
@@ -47,6 +47,22 @@ func main() {
 	// instantiate router
 	router := gin.Default()
 
+
+	// define go logger
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		// your custom format
+		return fmt.Sprintf("[%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+		)
+	}))
+
 	// define middlewares
 	router.Use(middlewares.CORSMiddleware())
 
