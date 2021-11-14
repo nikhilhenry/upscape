685a6bd11bf72b116f6b6a4f665f55d48600a0d1
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-11-14 20:26:32 +0530

    remove objectives icon

--- client/src/layouts/NavbarLayout.vue
+++ client/src/layouts/NavbarLayout.vue
@@ -1,92 +1,96 @@
 <template>
-<div>
-<nav class="navbar">
-  <ul class="navbar-nav">
-    <li class="logo">
-      <a href="#" class="nav-link">
-        <span class="link-text logo-text">Upscape</span>
-        <img src="../assets/bmo.svg">
-      </a>
-    </li>
-  <!-- navbar items -->
-      <!-- home -->
-      <li class="nav-item">
-        <router-link :to="{name:'Home'}" class="nav-link" :class="{'active-class':activeRoute=='Home'}">
-          <i class="las la-home icon"></i>
-          <span class="link-text">Home</span>
-        </router-link>
-      </li>
-      <!-- tasks -->
-      <li class="nav-item">
-        <router-link :to="{name:'Tasks'}" class="nav-link" :class="{'active-class':activeRoute=='Tasks'}">
-          <i class="las la-briefcase icon"></i>
-          <span class="link-text">Tasks</span>
-        </router-link>
-      </li>
-      <!-- objectives -->
-      <li class="nav-item">
-        <router-link :to="{name:'Objectives'}" class="nav-link" :class="{'active-class':activeRoute=='Objectives'}">
-          <i class="las la-certificate icon"></i>
-          <span class="link-text">Objectives</span>
-        </router-link>
-      </li>
+  <div>
+    <nav class="navbar">
+      <ul class="navbar-nav">
+        <li class="logo">
+          <a href="#" class="nav-link">
+            <span class="link-text logo-text">Upscape</span>
+            <img src="../assets/bmo.svg" />
+          </a>
+        </li>
+        <!-- navbar items -->
+        <!-- home -->
+        <li class="nav-item">
+          <router-link
+            :to="{ name: 'Home' }"
+            class="nav-link"
+            :class="{ 'active-class': activeRoute == 'Home' }"
+          >
+            <i class="las la-home icon"></i>
+            <span class="link-text">Home</span>
+          </router-link>
+        </li>
+        <!-- tasks -->
+        <li class="nav-item">
+          <router-link
+            :to="{ name: 'Tasks' }"
+            class="nav-link"
+            :class="{ 'active-class': activeRoute == 'Tasks' }"
+          >
+            <i class="las la-briefcase icon"></i>
+            <span class="link-text">Tasks</span>
+          </router-link>
+        </li>
 
-      <!-- settings -->
-      <li class="nav-item" >
-        <router-link :to="{name:'Settings'}" class="nav-link" :class="{'active-class':activeRoute=='Settings'}">
-          <i class="las la-cog icon"></i>
-          <span class="link-text">Settings</span>
-        </router-link>
-      </li>
-  </ul>
-</nav>
+        <!-- settings -->
+        <li class="nav-item">
+          <router-link
+            :to="{ name: 'Settings' }"
+            class="nav-link"
+            :class="{ 'active-class': activeRoute == 'Settings' }"
+          >
+            <i class="las la-cog icon"></i>
+            <span class="link-text">Settings</span>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
 
-<!-- router view -->
-<router-view class="main"/>
-</div>
+    <!-- router view -->
+    <router-view class="main" />
+  </div>
 </template>
 
 <script>
 export default {
-  name:'NavbarLayout',
-  computed:{
-    activeRoute(){
-      return this.$route.name
-    }
-  }  
-}
+  name: "NavbarLayout",
+  computed: {
+    activeRoute() {
+      return this.$route.name;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
-body{
+body {
   color: black;
   background-color: white;
   margin: 0;
   padding: 0;
 }
 
-.main{
+.main {
   margin-left: 5rem;
   padding: 1rem;
 }
 
 // active class
-.active-class{
+.active-class {
   background: $nav-bg-secondary;
-  filter:grayscale(0%)!important;
+  filter: grayscale(0%) !important;
   background: $nav-bg-secondary;
   color: $nav-text-secondary;
 }
 
-.navbar{
+.navbar {
   position: fixed;
   background-color: $nav-bg-primary;
   transition: width 200ms ease;
   z-index: 5;
 }
 
-.navbar-nav{
+.navbar-nav {
   list-style: none;
   padding: 0;
   margin: 0;
@@ -95,15 +99,15 @@ body{
   align-items: center;
   height: 100%;
 
-  .nav-item{
-    width: 100%
+  .nav-item {
+    width: 100%;
   }
 
-  .nav-item:last-child{
+  .nav-item:last-child {
     margin-top: auto;
   }
 
-  .nav-link{
+  .nav-link {
     display: flex;
     align-items: center;
     height: 5rem;
@@ -112,20 +116,20 @@ body{
     filter: grayscale(100%) opacity(0.7);
     transition: $transistion-speed;
 
-    &:hover{
-      filter:grayscale(0%);
+    &:hover {
+      filter: grayscale(0%);
       background: $nav-bg-secondary;
       color: $nav-text-secondary;
     }
 
-    .link-text{
+    .link-text {
       display: none;
       margin-left: 1rem;
     }
 
-    i{
+    i {
       min-width: 2rem;
-      margin:0 1.5rem;
+      margin: 0 1.5rem;
     }
   }
 }
@@ -149,12 +153,14 @@ body{
   letter-spacing: 0.3ch;
   width: 100%;
   transition: $transistion-speed;
-  .nav-link {filter: none!important;}
+  .nav-link {
+    filter: none !important;
+  }
 
-  img{
+  img {
     margin: 0 auto 0;
-    width:70%;
-    height:70%;
+    width: 70%;
+    height: 70%;
     filter: brightness(0) invert(0.8);
   }
 }
@@ -164,51 +170,49 @@ body{
   transition: $transistion-speed;
 }
 
-.logo-text
-{
+.logo-text {
   display: inline;
   transition: $transistion-speed;
 }
 
-
 // smaller screens
-@media only screen and(max-width: 600px){
-  .navbar{
+@media only screen and(max-width: 600px) {
+  .navbar {
     bottom: 0;
     width: 100vw;
     height: 5rem;
     border-top: 1px solid $nav-bg-secondary;
   }
 
-  .logo{
+  .logo {
     display: none;
   }
 
-  .navbar-nav{
+  .navbar-nav {
     flex-direction: row;
   }
 
-  .nav-link{
+  .nav-link {
     justify-content: center;
   }
 
-  .main{
-    margin:0;
-    margin-bottom:3rem;
+  .main {
+    margin: 0;
+    margin-bottom: 3rem;
   }
 }
 
 // large screen
-@media only screen and(min-width: 600px){
-  .navbar{
+@media only screen and(min-width: 600px) {
+  .navbar {
     top: 0;
-    width:5rem;
+    width: 5rem;
     height: 100vh;
     border-right: 1px solid $nav-bg-secondary;
-    &:hover{
-      width:16rem;
-      .link-text{
-        display: inline!important;
+    &:hover {
+      width: 16rem;
+      .link-text {
+        display: inline !important;
         transition: opacity $transistion-speed;
       }
     }
