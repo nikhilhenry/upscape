90ea3f9fe8555da134c0585abe0e30ec012b9e36
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-11-14 20:29:00 +0530

    refactor

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,27 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
 // module imports
-import user from './modules/UserStore'
-import task from './modules/TaskStore'
-import tag from './modules/TagStore'
-import objective from './modules/ObjectiveStore'
-import notification from './modules/NotificationStore'
+import user from "./modules/UserStore";
+import task from "./modules/TaskStore";
+import tag from "./modules/TagStore";
+import objective from "./modules/ObjectiveStore";
+import notification from "./modules/NotificationStore";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
     user,
     task,
     tag,
     objective,
-    notification
-  }
-})
+    notification,
+  },
+});
