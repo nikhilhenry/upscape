f24516c487a63f4497ca141e6c43fcae515af919
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-24 15:23:12 +0530

    rename to default layout

--- client-next/src/App.vue
+++ client-next/src/App.vue
@@ -8,14 +8,14 @@
 import { defineComponent, computed } from "vue";
 import { useRouter } from "vue-router";
 
-const defaultLayout = "Sidebar";
+const defaultLayout = "default";
 
 export default defineComponent({
   setup() {
     const { currentRoute } = useRouter();
 
     const layout = computed(
-      () => `${currentRoute.value.meta.layout || defaultLayout}Layout`
+      () => `${currentRoute.value.meta.layout || defaultLayout}-layout`
     );
 
     return {
