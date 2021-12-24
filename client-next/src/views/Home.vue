5fa95b77a4def880b9e96ae0db0a79c6590f7120
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-24 23:51:18 +0530

    create base home view

--- client-next/src/views/Home.vue
+++ client-next/src/views/Home.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="home">
+    <div class="container">
+      <!-- title bar -->
+      <div class="title-bar">
+        <h1 class="title">
+          {{ greeting }}, <br />
+          {{ firstName }} 👋
+        </h1>
+        <Avatar />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { computed, defineComponent, onMounted, ref } from "vue";
+import getGreeting from "../functions/getGreeting";
+import userStore from "../stores/user";
+
+export default defineComponent({
+  setup() {
+    const firstName = computed(() => {
+      return userStore.getters.getName;
+    });
+
+    const greeting = ref("");
+
+    onMounted(() => {
+      greeting.value = getGreeting();
+    });
+
+    return {
+      greeting,
+      firstName,
+    };
+  },
+});
+</script>
