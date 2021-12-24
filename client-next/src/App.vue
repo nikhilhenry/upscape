324d17c2083801d6478d03a3a21eb0c8cf2f5dfb
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-24 13:47:03 +0530

    implement scss variables

--- client-next/src/App.vue
+++ client-next/src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <router-view />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<style></style>
+<style lang="scss">
+:root {
+  background: $background;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $text-primary;
+  background: $background;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
