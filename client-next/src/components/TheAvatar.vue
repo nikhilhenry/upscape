c61145d317598bccb6363c5731b6dd1859f75fac
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-24 23:41:34 +0530

    create avatar component

---
+++ client-next/src/components/TheAvatar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="avatar">
+    <img :src="imgURL" alt="user_profile_pic" />
+  </div>
+</template>
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import userStore from "../stores/user";
+
+export default defineComponent({
+  setup() {
+    const imgURL = computed(() => {
+      return userStore.getters.getAvatarURL;
+    });
+
+    return {
+      imgURL,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.avatar {
+  height: 100px !important;
+  width: auto;
+  display: block !important;
+  img {
+    height: 100%;
+    width: auto;
+    border-radius: 35%;
+    object-fit: cover;
+  }
+}
+</style>
