6f545bcbc4be9bcc6347b4069a82fbd84ba19b01
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-24 14:51:27 +0530

    add axios defaults

--- client-next/src/main.ts
+++ client-next/src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
+import axios from "axios";
+
+// axios defaults
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.headers.common["auth_token"] =
+  localStorage.getItem("access_token") || "";
 
 const app = createApp(App);
 
