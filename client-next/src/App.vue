d2e6077f71eb25887d09dbf4aab81fe3ab6b4b29
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-24 15:03:11 +0530

    add layout logic

--- client-next/src/App.vue
+++ client-next/src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="app">
-    <router-view />
+    <component :is="layout"> <router-view /></component>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import { useRouter } from "vue-router";
+
+const defaultLayout = "default";
+
+export default defineComponent({
+  setup() {
+    const { currentRoute } = useRouter();
+
+    const layout = computed(
+      () => `${currentRoute.value.meta.layout || defaultLayout}-layout`
+    );
+
+    return {
+      layout,
+    };
+  },
+});
+</script>
 
 <style lang="scss">
 :root {
