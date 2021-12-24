2629f0708a64bb7fb33c8d879ddece0db1d9a8c7
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-24 14:55:06 +0530

    create router guards

--- client-next/src/router/index.ts
+++ client-next/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import userStore from "../stores/user";
 
 import Login from "../views/Login.vue";
 import Home from "../views/Home.vue";
@@ -21,6 +22,47 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes: routes,
+});
+
+// route guards
+router.beforeEach((to, from, next) => {
+  // get access token from store
+
+  const isAuthenticated = userStore.getters.isLoggedIn;
+
+  // if auth is required
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // check if user is authenticated
+    if (isAuthenticated) next();
+    // if not authenticated
+    else {
+      next({
+        path: "/login",
+        query: {
+          redirect: to.fullPath,
+        },
+      });
+    }
+  }
+  // if guest is required
+  else if (to.matched.some((record) => record.meta.requiresGuest)) {
+    // check if user is not authenticated
+    if (!isAuthenticated) next();
+    // if authenticated
+    else {
+      next({
+        path: "/",
+        query: {
+          redirect: to.fullPath,
+        },
+      });
+    }
+  }
+
+  // if nothing required
+  else {
+    next();
+  }
 });
 
 export default router;
