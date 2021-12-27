2e71cc306bef4efc4e1e9edd158a31f7c1293f88
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-27 19:06:08 +0530

    add base styling

--- client-next/src/components/ModalView.vue
+++ client-next/src/components/ModalView.vue
@@ -1,19 +1,27 @@
 <template>
-  <Dialog :open="isOpen" @close="setIsOpen">
-    <DialogOverlay />
+  <Dialog
+    :open="isOpen"
+    @close="setIsOpen"
+    class="fixed inset-0 z-10 overflow-y-auto"
+  >
+    <div class="flex items-center justify-center min-h-screen">
+      <DialogOverlay class="fixed inset-0 bg-black opacity-30" />
 
-    <DialogTitle>Deactivate account</DialogTitle>
-    <DialogDescription>
-      This will permanently deactivate your account
-    </DialogDescription>
+      <div class="relative max-w-sm mx-auto bg-white rounded">
+        <DialogTitle>Deactivate account</DialogTitle>
+        <DialogDescription>
+          This will permanently deactivate your account
+        </DialogDescription>
 
-    <p>
-      Are you sure you want to deactivate your account? All of your data will be
-      permanently removed. This action cannot be undone.
-    </p>
+        <p>
+          Are you sure you want to deactivate your account? All of your data
+          will be permanently removed. This action cannot be undone.
+        </p>
 
-    <button @click="setIsOpen(false)">Deactivate</button>
-    <button @click="setIsOpen(false)">Cancel</button>
+        <button @click="setIsOpen(false)">Deactivate</button>
+        <button @click="setIsOpen(false)">Cancel</button>
+      </div>
+    </div>
   </Dialog>
 </template>
 
