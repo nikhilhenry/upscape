b8bff606b5d1695647f9ab8e8bd12991b40a0d63
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-27 19:07:46 +0530

    use default modal

--- client-next/src/components/ModalView.vue
+++ client-next/src/components/ModalView.vue
@@ -1,48 +1,122 @@
 <template>
-  <Dialog
-    :open="isOpen"
-    @close="setIsOpen"
-    class="fixed inset-0 z-10 overflow-y-auto"
-  >
-    <div class="flex items-center justify-center min-h-screen">
-      <DialogOverlay class="fixed inset-0 bg-black opacity-30" />
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal">
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="min-h-screen px-4 text-center">
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0"
+            enter-to="opacity-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100"
+            leave-to="opacity-0"
+          >
+            <DialogOverlay class="fixed inset-0" />
+          </TransitionChild>
 
-      <div class="relative max-w-sm mx-auto bg-white rounded">
-        <DialogTitle>Deactivate account</DialogTitle>
-        <DialogDescription>
-          This will permanently deactivate your account
-        </DialogDescription>
+          <span class="inline-block h-screen align-middle" aria-hidden="true">
+            &#8203;
+          </span>
 
-        <p>
-          Are you sure you want to deactivate your account? All of your data
-          will be permanently removed. This action cannot be undone.
-        </p>
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <div
+              class="
+                inline-block
+                w-full
+                max-w-md
+                p-6
+                my-8
+                overflow-hidden
+                text-left
+                align-middle
+                transition-all
+                transform
+                bg-white
+                shadow-xl
+                rounded-2xl
+              "
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-gray-900"
+              >
+                Payment successful
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                  Your payment has been successfully submitted. We’ve sent you
+                  an email with all of the details of your order.
+                </p>
+              </div>
 
-        <button @click="setIsOpen(false)">Deactivate</button>
-        <button @click="setIsOpen(false)">Cancel</button>
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="
+                    inline-flex
+                    justify-center
+                    px-4
+                    py-2
+                    text-sm
+                    font-medium
+                    text-blue-900
+                    bg-blue-100
+                    border border-transparent
+                    rounded-md
+                    hover:bg-blue-200
+                    focus:outline-none
+                    focus-visible:ring-2
+                    focus-visible:ring-offset-2
+                    focus-visible:ring-blue-500
+                  "
+                  @click="closeModal"
+                >
+                  Got it, thanks!
+                </button>
+              </div>
+            </div>
+          </TransitionChild>
+        </div>
       </div>
-    </div>
-  </Dialog>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import {
+  TransitionRoot,
+  TransitionChild,
   Dialog,
   DialogOverlay,
   DialogTitle,
-  DialogDescription,
 } from "@headlessui/vue";
 
 export default defineComponent({
-  components: { Dialog, DialogOverlay, DialogTitle, DialogDescription },
+  components: {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogOverlay,
+    DialogTitle,
+  },
+
   setup() {
-    let isOpen = ref(true);
+    const isOpen = ref(true);
 
     return {
       isOpen,
-      setIsOpen(value: boolean) {
-        isOpen.value = value;
+      closeModal() {
+        isOpen.value = false;
       },
     };
   },
