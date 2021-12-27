91d8b761da680fb6100e8de30d37da086dd2d49b
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-27 11:40:55 +0530

    assign item indices

--- client-next/src/use/dragSort.ts
+++ client-next/src/use/dragSort.ts
@@ -1,21 +1,30 @@
 export default function useDragSort() {
+  let dragStartIndex: Number, dragEndIndex: Number;
+
   // list components
   function dragStart(event: any, index: Number) {
     event.dataTransfer.dropEffect = "move";
     event.dataTransfer.effectAllowed = "move";
 
-    console.log(index);
+    dragStartIndex = index;
   }
 
   // li funcitons
   // 'dragover'
   function dragOver() {}
   // 'drop'
-  function dragDrop(event: any, index: Number) {}
+  function dragDrop(event: any, index: Number) {
+    dragEndIndex = index;
+    swapItems(dragStartIndex, dragEndIndex);
+  }
   // 'dragenter'
   function dragEnter() {}
   // 'dragleave'
   function dragLeave() {}
+
+  function swapItems(fromIndex: Number, toIndex: Number) {
+    console.log(fromIndex, toIndex);
+  }
 
   return {
     dragStart,
