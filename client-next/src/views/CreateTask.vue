b2f264774e81197a6992a3d7fb7a0f207f624c44
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-27 23:18:12 +0530

    format field

--- client-next/src/views/CreateTask.vue
+++ client-next/src/views/CreateTask.vue
@@ -12,12 +12,29 @@
       </div>
       <div class="mb-6">
         <label class="label">Task Duration</label>
-        <input
-          type="number"
-          class="input-field"
-          placeholder="name@flowbite.com"
-          required
-        />
+        <input type="number" class="input-field" placeholder="0" required />
+      </div>
+      <div class="flex items-start mb-6">
+        <div class="flex items-center h-5">
+          <input
+            type="checkbox"
+            class="
+              w-4
+              h-4
+              rounded
+              border
+              bg-gray-700
+              border-gray-600
+              focus:ring-offset-sky-500
+            "
+            required
+          />
+        </div>
+        <div class="ml-3 text-sm">
+          <label for="remember" class="font-medium text-gray-300"
+            >Highlight of the Day</label
+          >
+        </div>
       </div>
       <button
         type="submit"
@@ -53,10 +70,10 @@ export default defineComponent({
 
 <style lang="scss">
 .input-field {
-  @apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
+  @apply bg-gray-50 border border-gray-300 text-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
 }
 
 .label {
-  @apply block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300;
+  @apply block mb-2 text-sm font-medium text-gray-300;
 }
 </style>
