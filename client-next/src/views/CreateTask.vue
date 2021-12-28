3730383cf2adaf50e16cd4784c64cb5d88ed5c6c
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-28 14:39:48 +0530

    add loading spinner

--- client-next/src/views/CreateTask.vue
+++ client-next/src/views/CreateTask.vue
@@ -46,6 +46,7 @@
       <button
         type="submit"
         class="
+          inline-flex
           text-white
           bg-blue-700
           hover:bg-blue-800
@@ -61,6 +62,26 @@
           dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
         "
       >
+        <svg
+          v-if="isLoading"
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
         Submit
       </button>
     </form>
@@ -68,7 +89,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { useRoute } from "vue-router";
 import ModalView from "../components/ModalView.vue";
 import { postTask } from "../services/taskService";
@@ -78,6 +99,7 @@ export default defineComponent({
   components: { ModalView },
   setup() {
     const route = useRoute();
+    const isLoading = ref(false);
 
     const submitForm = async (event: any) => {
       const formData: any = new FormData(event.target);
@@ -94,12 +116,15 @@ export default defineComponent({
         is_tomorrow: route.query.range == "tomorrow" ? true : false,
       };
 
+      isLoading.value = !isLoading.value;
       const task = await postTask(taskRequest);
+      isLoading.value = !isLoading.value;
       if (task) taskStore.storeTask(task);
     };
 
     return {
       submitForm,
+      isLoading,
     };
   },
 });
