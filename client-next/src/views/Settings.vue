7a049066f09ecaa2ae4f2e6300bb04fa8bd2e5c2
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-28 15:23:44 +0530

    add base form styling

--- client-next/src/views/Settings.vue
+++ client-next/src/views/Settings.vue
@@ -6,6 +6,37 @@
         <h1 class="title">Settings</h1>
         <TheAvatar />
       </div>
+      <section class="mt-16 grid grid-cols-2 gap-x-16">
+        <div class="mb-6">
+          <label class="label">Change Username</label>
+          <input
+            type="text"
+            class="input-field"
+            placeholder="New Name"
+            required
+            name="taskName"
+          />
+        </div>
+        <div class="mb-6">
+          <label class="label">Update Password</label>
+          <label class="sub-label">Current Password</label>
+          <input
+            type="text"
+            class="input-field"
+            placeholder="New Name"
+            required
+            name="taskName"
+          />
+          <label class="sub-label">New Password</label>
+          <input
+            type="text"
+            class="input-field"
+            placeholder="New Name"
+            required
+            name="taskName"
+          />
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -20,3 +51,17 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss">
+.input-field {
+  @apply w-9/12 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white;
+}
+
+.label {
+  @apply block my-5 text-xl font-medium text-gray-300;
+}
+
+.sub-label {
+  @apply block my-3 text-lg font-light text-gray-300;
+}
+</style>
