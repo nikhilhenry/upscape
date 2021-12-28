fac294606d54d1c920f1fa12ed73d8763996d0dd
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-28 14:57:32 +0530

    add base elements

--- client-next/src/views/Settings.vue
+++ client-next/src/views/Settings.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Settings</h1>
+  <div class="home">
+    <div class="container">
+      <!-- title bar -->
+      <div class="title-bar">
+        <h1 class="title">Settings</h1>
+        <TheAvatar />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TheAvatar from "../components/TheAvatar.vue";
+
+export default defineComponent({
+  components: {
+    TheAvatar,
+  },
+});
+</script>
