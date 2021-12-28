6a0c476b01a190b045c915306286fcbb63ae334e
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-28 14:57:20 +0530

    fix route name

--- client-next/src/router/index.ts
+++ client-next/src/router/index.ts
@@ -34,7 +34,7 @@ const routes: RouteRecordRaw[] = [
     ],
   },
   {
-    path: "/",
+    path: "/settings",
     name: "Settings",
     component: () => import("../views/Settings.vue"),
     meta: { requiresAuth: true },
