8f4f36d0dadc7f155809ed80f8a74aa9987d3e87
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-29 23:30:17 +0530

    create base default theme

--- client-next/tailwind.config.js
+++ client-next/tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
       textColor: {
         skin: {
           base: "var(--color-text-base)",
+          muted: "var(--color-text-muted)",
+          inverted: "var(--color-text-inverted)",
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: "var(--color-fill)",
+          muted: "var(--color-muted)",
+          elevate: "var(--color-elevated)",
+          "button-accent": "var(--color-button-accent)",
+          "button-accent-hover": "var(--color-button-accent-hover)",
+          "button-muted": "var(--color-button-muted)",
+          "button-elevated": "var(--color-button-elevated)",
+          "button-elevated-hover": "var(--color-button-elevated-hover)",
         },
       },
     },
