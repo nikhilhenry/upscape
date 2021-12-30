18b7cf3d4a078dd4333a7236404e8e4b180cd397
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-30 16:31:06 +0530

    fix struct type

--- models/Actionable.go
+++ models/Actionable.go
@@ -6,7 +6,7 @@ type Actionable struct {
 	ID        primitive.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name      string               `json:"name,omitempty" bson:"name,omitempty" validate:"required,max=16,alpha"`
 	Oid       int                  `json:"id,omitempty" bson:"id,omitempty"`
-	Inbox     bool                 `json:"inbox,omitempty" bson:"inbox,omitempty" validate:"required,bool"`
+	Inbox     bool                 `json:"inbox,omitempty" bson:"inbox,omitempty"`
 	TaskIDs   []primitive.ObjectID `json:"taskIDs,omitempty" bson:"taskIDs,omitempty"`
-	Completed bool                 `json:"completed" bson:"completed" validate:"bool"`
+	Completed bool                 `json:"completed" bson:"completed"`
 }
