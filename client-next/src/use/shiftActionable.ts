438de7f37e05015e2f45203008cb3af3695b0dd4
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-31 00:18:48 +0530

    create shift function

---
+++ client-next/src/use/shiftActionable.ts
@@ -0,0 +1,25 @@
+import {
+  deleteActionable,
+  updateActionable,
+} from "../services/actionableService";
+import { Actionable } from "../types/actionableTypes.interface";
+import actionableStore from "../stores/actionable";
+
+export default function useShiftActionable() {
+  return async (actionable: Actionable) => {
+    if (!actionable._id) return;
+    // create new actionable
+    const shiftedActionable = await updateActionable(actionable._id, {
+      inbox: !actionable.inbox,
+    });
+
+    if (shiftedActionable == null) return;
+    if (shiftedActionable.inbox) {
+      actionableStore.storeInboxItem(actionable);
+      actionableStore.deleteWeeklyItem(actionable._id);
+    } else {
+      actionableStore.storeWeeklyItem(actionable);
+      actionableStore.deleteInboxItem(actionable._id);
+    }
+  };
+}
