7c49417b961700c92235c528ad84514a78f6669a
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-30 11:38:33 +0530

    implement base dropdown

---
+++ client-next/src/components/TaskItemMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ml-8">
+    <Menu as="div" class="relative inline-block text-left">
+      <MenuButton><i class="fas fa-chevron-down"></i></MenuButton>
+      <MenuItems>
+        <MenuItem v-slot="{ active }">
+          <a :class="{ 'bg-blue-500': active }" href="/account-settings">
+            Account settings
+          </a>
+        </MenuItem>
+        <MenuItem v-slot="{ active }">
+          <a :class="{ 'bg-blue-500': active }" href="/account-settings">
+            Documentation
+          </a>
+        </MenuItem>
+        <MenuItem disabled>
+          <span class="opacity-75">Invite a friend (coming soon!)</span>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+</script>
