7b21206dcc1c0f53acc05bc22587b9b0f9ac2e13
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-30 12:20:49 +0530

    add transistion

--- client-next/src/components/TaskItemMenu.vue
+++ client-next/src/components/TaskItemMenu.vue
@@ -4,63 +4,74 @@
       <MenuButton class="hover:text-skin-inverted transition-colors"
         ><i class="fas fa-chevron-down"></i
       ></MenuButton>
-      <MenuItems
-        class="
-          absolute
-          right-0
-          mt-2
-          ml-5
-          origin-top-right
-          bg-skin-base
-          rounded-md
-          shadow-lg
-          ring-1 ring-black ring-opacity-5
-          focus:outline-none
-          z-10
-        "
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-in"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
       >
-        <div class="px-2 py-2 flex flex-col w-max text-lg">
-          <MenuItem v-slot="{ active }">
-            <button
-              :class="[
-                active
-                  ? 'bg-skin-fill bg-opacity-50 text-skin-base'
-                  : 'text-skin-muted',
-                'group flex rounded-md items-center w-full px-2 py-2',
-              ]"
-            >
-              <i class="fas fa-paper-plane w-5 h-5 mr-2 text-skin-inverted"></i>
-              Shift tomorrow
-            </button>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <button
-              :class="[
-                active
-                  ? 'bg-skin-fill bg-opacity-50 text-skin-base'
-                  : 'text-skin-muted',
-                'group flex rounded-md items-center w-full px-2 py-2',
-              ]"
-            >
-              <i class="fas fa-play w-5 h-5 mr-2 text-skin-inverted"></i>
-              Start Timer
-            </button>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <button
-              :class="[
-                active
-                  ? 'bg-skin-fill bg-opacity-50 text-skin-base'
-                  : 'text-skin-muted',
-                'group flex rounded-md items-center w-full px-2 py-2',
-              ]"
-            >
-              <i class="fas fa-backspace w-5 h-5 mr-2 text-skin-inverted"></i>
-              Delete Task
-            </button>
-          </MenuItem>
-        </div>
-      </MenuItems>
+        <MenuItems
+          class="
+            absolute
+            right-0
+            mt-2
+            ml-5
+            origin-top-right
+            bg-skin-base
+            rounded-md
+            shadow-lg
+            ring-1 ring-black ring-opacity-5
+            focus:outline-none
+            z-10
+          "
+        >
+          <div class="px-2 py-2 flex flex-col w-max text-lg">
+            <MenuItem v-slot="{ active }">
+              <button
+                :class="[
+                  active
+                    ? 'bg-skin-fill bg-opacity-50 text-skin-base'
+                    : 'text-skin-muted',
+                  'group flex rounded-md items-center w-full px-2 py-2',
+                ]"
+              >
+                <i
+                  class="fas fa-paper-plane w-5 h-5 mr-2 text-skin-inverted"
+                ></i>
+                Shift tomorrow
+              </button>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+              <button
+                :class="[
+                  active
+                    ? 'bg-skin-fill bg-opacity-50 text-skin-base'
+                    : 'text-skin-muted',
+                  'group flex rounded-md items-center w-full px-2 py-2',
+                ]"
+              >
+                <i class="fas fa-play w-5 h-5 mr-2 text-skin-inverted"></i>
+                Start Timer
+              </button>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+              <button
+                :class="[
+                  active
+                    ? 'bg-skin-fill bg-opacity-50 text-skin-base'
+                    : 'text-skin-muted',
+                  'group flex rounded-md items-center w-full px-2 py-2',
+                ]"
+              >
+                <i class="fas fa-backspace w-5 h-5 mr-2 text-skin-inverted"></i>
+                Delete Task
+              </button>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </transition>
     </Menu>
   </div>
 </template>
