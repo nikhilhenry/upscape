34e5c7bfedabd94126c621fc3762c0421063c1e5
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-30 16:58:26 +0530

    create actionable request type

--- models/Actionable.go
+++ models/Actionable.go
@@ -1,6 +1,8 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Actionable struct {
 	ID        primitive.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
@@ -9,4 +11,11 @@ type Actionable struct {
 	Inbox     bool                 `json:"inbox" bson:"inbox"`
 	TaskIDs   []primitive.ObjectID `json:"taskIDs,omitempty" bson:"taskIDs,omitempty"`
 	Completed bool                 `json:"completed" bson:"completed"`
+}
+
+type ActionableUpdate struct {
+	Name      string `json:"name,omitempty" validate:"required,max=20" bson:"name,omitempty"`
+	Oid       *int   `json:"id,omitempty" bson:"id,omitempty"`
+	Inbox     bool   `json:"inbox" bson:"inbox"`
+	Completed *bool  `json:"completed,omitempty" bson:"completed,omitempty"`
 }
