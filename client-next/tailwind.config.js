43e51c36ec85ed40371dd9f7cdd683f73e805841
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-30 09:04:04 +0530

    implement opacity function

--- client-next/tailwind.config.js
+++ client-next/tailwind.config.js
@@ -1,9 +1,9 @@
 function withOpacity(variableName) {
   return ({ opacityValue }) => {
     if (opacityValue !== undefined) {
-      return `rgba(${variableName},${opacityValue})`;
+      return `rgba(var(${variableName}), ${opacityValue})`;
     }
-    return `rgb(${variableName})`;
+    return `rgb(var(${variableName}))`;
   };
 }
 
@@ -13,25 +13,21 @@ module.exports = {
     extend: {
       textColor: {
         skin: {
-          base: "var(--color-text-base)",
-          muted: "var(--color-text-muted)",
-          inverted: "var(--color-text-inverted)",
+          base: withOpacity("--color-text-base"),
+          muted: withOpacity("--color-text-muted"),
+          inverted: withOpacity("--color-text-inverted"),
         },
       },
       backgroundColor: {
         skin: {
-          fill: withOpacity("var(--color-fill)"),
-          muted: withOpacity("var(--color-muted)"),
-          elevated: withOpacity("var(--color-elevated)"),
-          "button-accent": withOpacity("var(--color-button-accent)"),
-          "button-accent-hover": withOpacity(
-            "var(--color-button-accent-hover)"
-          ),
-          "button-muted": withOpacity("var(--color-button-muted)"),
-          "button-elevated": withOpacity("var(--color-button-elevated)"),
-          "button-elevated-hover": withOpacity(
-            "var(--color-button-elevated-hover)"
-          ),
+          fill: withOpacity("--color-fill"),
+          muted: withOpacity("--color-muted"),
+          elevated: withOpacity("--color-elevated"),
+          "button-accent": withOpacity("--color-button-accent"),
+          "button-accent-hover": withOpacity("--color-button-accent-hover"),
+          "button-muted": withOpacity("--color-button-muted"),
+          "button-elevated": withOpacity("--color-button-elevated"),
+          "button-elevated-hover": withOpacity("--color-button-elevated-hover"),
         },
       },
     },
