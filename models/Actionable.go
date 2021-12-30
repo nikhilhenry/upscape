3b4d275b6f6776172aaa8b9fcfb617fe6b548673
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-30 16:32:48 +0530

    fix struct validation

--- models/Actionable.go
+++ models/Actionable.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Actionable struct {
 	ID        primitive.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name      string               `json:"name,omitempty" bson:"name,omitempty" validate:"required,max=16,alpha"`
+	Name      string               `json:"name,omitempty" bson:"name,omitempty" validate:"required,max=16"`
 	Oid       int                  `json:"id,omitempty" bson:"id,omitempty"`
 	Inbox     bool                 `json:"inbox,omitempty" bson:"inbox,omitempty"`
 	TaskIDs   []primitive.ObjectID `json:"taskIDs,omitempty" bson:"taskIDs,omitempty"`
