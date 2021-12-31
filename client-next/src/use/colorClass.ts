87c4a333b17ae62e96fe94535044c2461fb55044
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-12-31 19:22:32 +0530

    make non computed

--- client-next/src/use/colorClass.ts
+++ client-next/src/use/colorClass.ts
@@ -1,14 +1,12 @@
-import { computed } from "vue";
-
 export default function useColorClass(duration: Number) {
   // colors class
-  const colorClass = computed(() => {
+  const colorClass = () => {
     if (duration < 2) return "light";
     if (duration < 4.5) return "medium";
     if (duration < 6) return "medium-heavy";
     if (duration >= 6) return "heavy";
     return "none";
-  });
+  };
 
   return colorClass;
 }
