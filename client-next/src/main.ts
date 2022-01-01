9f2d2cf5d46da2eaee9ab20bab24e5a75e0ead92
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-01-01 12:23:03 +0530

    implement interceptor

--- client-next/src/main.ts
+++ client-next/src/main.ts
@@ -5,12 +5,16 @@ import axios from "axios";
 import SidebarLayout from "./layouts/SidebarLayout.vue";
 import EmptyLayout from "./layouts/EmptyLayout.vue";
 import "./assets/tailwind.css";
+import { useErrorInterceptor } from "./use/axiosInterceptor";
 
 // axios defaults
 if (import.meta.env.VITE_API_URL)
   axios.defaults.baseURL = String(import.meta.env.VITE_API_URL);
 axios.defaults.headers.common["auth_token"] =
   localStorage.getItem("auth_token") || "";
+
+// axios error interceptors
+useErrorInterceptor(router);
 
 const app = createApp(App);
 
